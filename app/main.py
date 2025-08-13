b36fce7e4ddf4e6333eed13274cca69f13a4b2a5
Kami <josephkiarie561@gmail.com>
2025-08-13 13:12:41 +0300

    bootstrap icons

--- app/main.py
+++ app/main.py
@@ -14,3 +14,34 @@ diabetes_scaler = load_pickle("models/diabetes_scaler.sav")
 heart_model = load_pickle("models/heart_model.sav")
 parkinsons_model = load_pickle("models/parkinsons_model.sav")
 parkinsons_scaler = load_pickle("models/parkinsons_scaler.sav")
+
+# sidebar navigation
+with st.sidebar:
+    selected = option_menu(
+        "Multiple Disease Diagnosis",
+        [
+            "Diabetes",
+            "Heart Disease",
+            "Parkinsons"
+        ],
+        icons=[
+            "capsule",
+            "heart-pulse",
+            "person-standing"
+        ],
+        default_index=0
+    )
+
+# navigating selected pages
+
+# diabetes disease page
+if (selected=="Diabetes"):
+    st.title("Diabetes Disease Diagnosis.")
+
+# heart disease page
+if (selected=="Heart Disease"):
+    st.title("Heart Disease Diagnosis.")
+
+# parkinsons disease page
+if (selected=="Parkinsons"):
+    st.title("Parkinsons Disease Diagnosis.")
